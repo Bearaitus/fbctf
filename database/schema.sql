5b1c2150fb2359b2cc0333fb71d33da0eefbe4af
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-09-13 17:25:14 +0300

    замена языка

--- database/schema.sql
+++ database/schema.sql
@@ -259,7 +259,7 @@ INSERT INTO `configuration` (field, value, description) VALUES("login_google", "
 INSERT INTO `configuration` (field, value, description) VALUES("password_type", "1", "(Integer) Type of passwords: See table password_types");
 INSERT INTO `configuration` (field, value, description) VALUES("default_bonus", "30", "(Integer) Default value for bonus in levels");
 INSERT INTO `configuration` (field, value, description) VALUES("default_bonusdec", "10", "(Integer) Default bonus decrement in levels");
-INSERT INTO `configuration` (field, value, description) VALUES("language", "en", "(String) Language of the system");
+INSERT INTO `configuration` (field, value, description) VALUES("language", "ru", "(String) Language of the system");
 INSERT INTO `configuration` (field, value, description) VALUES("livesync", "0", "(Boolean) LiveSync functionality");
 INSERT INTO `configuration` (field, value, description) VALUES("livesync_auth_key", "", "(String) Optional LiveSync Auth Key");
 INSERT INTO `configuration` (field, value, description) VALUES("custom_logo", "0", "(Boolean) Custom branding logo");
